6e75f94d888ac45268ec79bc06107171d39d87f2
kodiakshuksan <74173891+kodiakshuksan@users.noreply.github.com>
2021-05-06 01:15:36 -0700

    connected first component

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import EmployeeDirectory from './employeeDirectory';
+
+function App() {
+  return (
+    <>
+   
+     <div>
+      
+      <EmployeeDirectory /> 
+    
+    </div>
+    </>
+  );
+}
+
+export default App;
