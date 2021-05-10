4270b5bd4e871c926eb5f903e02479cf8ee2e455
kodiakshuksan <74173891+kodiakshuksan@users.noreply.github.com>
2021-05-09 23:39:52 -0700

    added get users button

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-
 import './App.css';
 import EmployeeDirectory from './employeeDirectory';
 import Card from "./Card";
+import Button from "./Button";
+import React, { useState } from 'react';
 
-function App() {
+ 
+const App = () => {
+ // const [userData, setUserData] = useState([]);
+ // const [loading, setLoading] = useState(false);
+  const [activeUser, setActiveUser] = useState(false);
+  //const [activeLink, setActiveLink] = useState(0);
+
+  const onClickHandler = ()=>{
+    console.log("button is working")
+  }
   return (
     <>
    
      <div>
       
       <EmployeeDirectory /> 
+      <Button isActive={activeUser} clicked={onClickHandler }/>
       
-      <button>show all managers</button>
-      <button>show all female employees</button>
-      <button>show all plumbers</button>
+      <br>
+      </br>
+      <Card />
+      
+      <br>
+      </br>
+      <Card />
+      <br>
+      </br>
       <Card />
     
     </div>
